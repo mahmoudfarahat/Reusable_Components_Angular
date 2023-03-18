39ce99ba7b722eebc0a238c3f99384d577ec84a2
Mahmoud Farahat <mahmoud.ped@gmail.com>
2023-03-18 22:15:17 +0200

    first commit

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+data = [
+  {name:'James', age:24 , job:'Designer' , employed:true},
+  {name:'Jill', age:21 , job:'Engineer' , employed:false},
+  {name:'Mahmoud', age:25 , job:'Designer', employed:true},
+  {name:'Jill', age:21 , job:'Engineer' , employed:false},
+  {name:'Mahmoud', age:25 , job:'Designer', employed:true},
+  {name:'James', age:24 , job:'Designer' , employed:true},
+  {name:'Mahmoud', age:25 , job:'Designer', employed:true},
+]
+
+headers =[
+  {key : 'name', label:'Name'},
+  {key : 'age', label:'Age'},
+  {key : 'job', label:'Job'},
+
+
+]
 
   constructor() { }
 
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,20 +1,30 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { TableComponent } from './table/table.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
   declarations: [
-    CollectionsHomeComponent
+    CollectionsHomeComponent,
+    TableComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   exports:[
-     
+
   ]
 })
 export class CollectionsModule { }
